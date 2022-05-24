e301f9977f5e2383a92e8fd1d3485ff0039849ae
Ihsanul-Ikhwan <65582832+Ihsanul-Ikhwan@users.noreply.github.com>
2022-05-24 09:49:42 +0700

    Initial commit

---
+++ app/src/main/java/com/ihsan/ngetravel/FirstPageActivity.java
@@ -0,0 +1,27 @@
+package com.ihsan.ngetravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstPageActivity extends AppCompatActivity {
+Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FirstPageActivity.this, MainActivity.class);
+                startActivity(intent);
+                           }
+        });
+    }
+}
---
+++ app/src/main/java/com/ihsan/ngetravel/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.ihsan.ngetravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timer = new Thread(){
+            public void run() {
+                try {
+                    sleep(1500);
+                } catch (Exception er) {
+                    er.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreenActivity.this, FirstPageActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+    }
+}
