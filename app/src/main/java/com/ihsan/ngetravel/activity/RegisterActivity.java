44619e47ece401cf8187b679b1194a85d4bccfd6
Ihsanul-Ikhwan <65582832+Ihsanul-Ikhwan@users.noreply.github.com>
2022-06-03 07:45:42 +0700

    RegisterActivity

--- app/src/main/java/com/ihsan/ngetravel/activity/RegisterActivity.java
+++ app/src/main/java/com/ihsan/ngetravel/activity/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.ihsan.ngetravel.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -66,8 +67,11 @@ public class RegisterActivity extends AppCompatActivity {
                 email.setText("");
                 password.setText("");
 
-                Toast.makeText(getApplicationContext(), response.toString()
-                        , Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), response.toString()
+//                        , Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
             }
 
             @Override
